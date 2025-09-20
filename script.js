6dc5a768a96a3d568a930d118dbd8dee30417e94
Francisco0102-code <francisco.araujo927@aluno.ce.gov.br>
2025-09-20 18:09:51 -0300

    menu

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-// MENU MOBILE
 const menuToggle = document.getElementById("menu-toggle");
 const nav = document.getElementById("nav");
+const navLinks = nav.querySelectorAll("a"); // pega todos os links do menu
 
+// Abrir/fechar menu ao clicar no botão
 menuToggle.addEventListener("click", () => {
   nav.classList.toggle("active");
+});
+
+// Fechar menu ao clicar em qualquer link
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
 });
