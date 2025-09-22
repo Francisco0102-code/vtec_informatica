a63bfcbbdedbb216371c6f7de7551c684f635b48
Francisco0102-code <francisco.araujo927@aluno.ce.gov.br>
2025-09-22 16:21:33 -0300

    melhorando pequenas coisas

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-// Menu mobile
-const toggle = document.getElementById("menu-toggle");
+const menuToggle = document.getElementById("menu-toggle");
 const nav = document.getElementById("nav");
-toggle.addEventListener("click", () => {
+const navLinks = nav.querySelectorAll("a"); // pega todos os links do menu
+
+// Abrir/fechar menu ao clicar no botão
+menuToggle.addEventListener("click", () => {
   nav.classList.toggle("active");
 });
+
+// Fechar menu ao clicar em qualquer link
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+});
+
+
+
 
 // Função para detectar se elemento está visível
 function isInViewport(el) {
